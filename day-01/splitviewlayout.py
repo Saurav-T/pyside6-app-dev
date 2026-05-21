807ae1bd81dc0cbd2b46a2fbcc56147ef5d6f081
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-05-21 21:44:44 +0545

    feat: add split view layout

---
+++ day-01/splitviewlayout.py
@@ -0,0 +1,28 @@
+from PySide6.QtWidgets import (
+    QApplication, QWidget, 
+    QSplitter, QHBoxLayout, 
+    QListWidget, QTextEdit)
+
+from PySide6.QtCore import Qt
+
+app = QApplication([])
+window = QWidget()
+
+splitter = QSplitter(Qt.Vertical)
+
+menu = QListWidget()
+menu.addItems(["Home", "Profile", "Settings"])
+
+content = QTextEdit()
+
+splitter.addWidget(menu)
+splitter.addWidget(content)
+
+layout = QHBoxLayout()
+layout.addWidget(splitter)
+
+window.setLayout(layout)
+window.resize(800, 500)
+window.show()
+
+app.exec()
