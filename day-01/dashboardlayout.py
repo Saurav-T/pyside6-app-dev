aa6abf5659609f228b2ea2e3620b479553812f19
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-05-21 12:14:24 +0545

    feat: add pyside dashboard layout

---
+++ day-01/dashboardlayout.py
@@ -0,0 +1,34 @@
+from PySide6.QtWidgets import (
+    QApplication, QWidget,
+    QVBoxLayout, QHBoxLayout,
+    QPushButton, QLabel
+)
+
+from PySide6.QtCore import Qt
+
+app = QApplication([])
+window = QWidget()
+
+home = QPushButton("Home")
+profile = QPushButton("Profile")
+settings = QPushButton("Settings")
+
+sidebar = QVBoxLayout()
+sidebar.addWidget(home)
+sidebar.addWidget(profile)
+sidebar.addWidget(settings)
+sidebar.addStretch()
+
+content_label = QLabel("Welcome")
+
+main = QHBoxLayout()
+
+main.addLayout(sidebar,1)
+main.addWidget(content_label, 4, alignment=Qt.AlignCenter)
+
+window.setLayout(main)
+
+window.resize(700, 400)
+window.show()
+
+app.exec()
